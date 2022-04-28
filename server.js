254d50cbb8d498642aa447093b23bc907745df88
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-27 23:27:08 -0700

    add base files

---
+++ server.js
@@ -0,0 +1,15 @@
+// Use Express.js (installed)
+// Deploy via Heroku??
+
+// Open the Note-taker link--> takes you to landing page with button to NOTES page 
+// Click on the NOTES page link 
+    // Opens with the notes on the LEFT, and empty area for NEW notes on the RIGHT --> Already done
+// New note and title entered --> DONE 
+    // SAVE icon appears
+// Click the SAVE icon --> DONE
+    // Note is saved, MOVES to the RIGHT
+// Click the note on the LEFT side, opens on the RIGHT side 
+// Click the '+' icon to create a NEW note 
+// GET/notes for notes.html
+// GET* for index.html 
+// API routes: GET/api and POST/api
