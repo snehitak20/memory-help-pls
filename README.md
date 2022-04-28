30dbb014a94726c88439f587123c7e604c007462
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-28 15:26:57 -0700

    switched apiRoutes back to the working code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # memory-help-pls
+
+## Deployed Webpage 
+
+[Link to webpage](https://memory-help-pls.herokuapp.com/)
+
+## Description  
+
+The goal of this project is to build a note-taking app. The note is able to be saved, can be accessed again to view the note, and can also be deleted. New notes can be added at any time. The front end of this project is provided, so the main objective is to build the entire back-end of the project. 
+
+Within the backend, JavaScript is the star of this project, 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,41 @@
-const fs = require('fs');
-const noteData = require('../db/db.json');
-const uniqid = require('uniqid');
-const db = 'db/db.json';
-
+// Dependecies 
+const db ='db/db.json';
+const fs = require ('fs');
+// Immediately exports the function called (app) and does the following:
 module.exports = (app) => {
+        // GET request: should read the db.json file and return ALL saved notes as JSON
+        app.get('/api/notes', (req, res) => {
+            let savedNotes = fs.readFileSync(db) 
+            return res.json(JSON.parse(savedNotes));
+        });
 
-    app.get('/api/notes', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('./db/db.json'));
-        res.json(db);
-    });
+        // POST request: receives a new note to save onto the body of the request--> adds it to db.json file and returns the new note to the client 
+        app.post('/api/notes', (req, res) => {
+            let savedNotes = fs.readFileSync(db) 
+            savedNotes = JSON.parse(savedNotes);
+            savedNotes.push(req.body);
+            
+            // Creates a new id for each saved note
+            for (let i = 0; i < savedNotes.length; i++) {
+                savedNotes[i].id = i+1;
+            }
+    
+            fs.writeFileSync(db, JSON.stringify(savedNotes));
+            return res.send(db, savedNotes);
+        });
 
-    app.post('/api/notes', (req, res) => {
-        let notes = JSON.parse(fs.readFileSync('db/db.json'));
-        res.json(notes);
+        // DELETE request: 
+        app.delete('/api/notes/:id', (req, res) => {
+            const {id} = req.params;
+            let notes = fs.readFileSync(db) 
+            notes = JSON.parse(notes);
+            let indexToDelete = notes.filter((each) => each.id != id);
+    
+            if(!indexToDelete) {
+                return res.status(404).json({error: 'No note with that id'}); 
+            }
 
-        const newNotes = {
-            title: req.body.title,
-            text: req.body.text,
-            id: uniqid(),
-        };
-
-        notes.push(newNotes);
-        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-        res.json(notes);
-    });
-
-    app.delete('/api/notes/:id', (req, res) => {
-        const {id} = req.params;
-        let notes = fs.readFileSync(db)
-        notes = JSON.parse(notes);
-        let indexToDelete = notes.filter((each) => each.id !=id);
-
-        if(!indexToDelete) {
-            return res.status(404).json({error: 'note does not exist'});
-        }
-
-        fs.writeFileSync(db, JSON.stringify(indexToDelete));
+            fs.writeFileSync(db, JSON.stringify(indexToDelete))
             res.send(db, indexToDelete);
         });
 };
